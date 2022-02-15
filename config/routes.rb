0771d57efa2b6ab3bb2a8e6627304471c7f962d4
Georges Chami <g.chami90@gmail.com>
2022-02-15 17:05:13 -0500

    completed the second challenge

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,19 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # See all task
+  get 'tasks', to: 'tasks#index'
+
+  # Create a task
+  get 'tasks/new', to: 'tasks#new'
+
+  # See details about one task
+  get 'tasks/:id', to: 'tasks#show', as: :task
+  post 'tasks', to: 'tasks#create', as: :create
+
+  # Update a task
+  get 'tasks/:id/edit', to: 'tasks#edit', as: :edit
+  patch 'tasks/:id', to: 'tasks#update'
+
+  # delete a task
+  delete 'tasks/:id', to: 'tasks#destroy', as: :delete
 end
--- db/schema.rb
+++ db/schema.rb
@@ -14,7 +14,7 @@ ActiveRecord::Schema.define(version: 2022_02_15_194100) do
 
   create_table "tasks", force: :cascade do |t|
     t.string "title"
-    t.text "detail"
+    t.text "details"
     t.boolean "completed"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
